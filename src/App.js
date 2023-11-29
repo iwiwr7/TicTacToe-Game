92333dba653c0c99fd30fbeb44615e158c6517b8
Ivan Ivanov <iwiwr7@gmail.com>
2023-11-29 14:53:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TicTacToe from './Components/TicTac-Toe/TicTac-Toe';
+
+function App() {
+  return (
+    <div className="App">
+
+      <TicTacToe />
+      
+
+    </div>
+  );
+}
+
+export default App;
